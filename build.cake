c5ec56f96785ed56772858684c1e301c5067bcc3
Seth Hendrick <seth@shendrick.net>
2021-02-07 16:01:59 -0500

    Modified docker and jenkins to deploy Docker images and manifest file.
    ... let's see what happens!

--- build.cake
+++ build.cake
@@ -12,9 +12,19 @@ string target = Argument( "target", buildTarget );
 const string linuxX64Rid = "linux-x64";
 const string linuxArmRid = "linux-arm";
 
+const string linuxX64DockerArch = "linux-amd64";
+const string linuxArmDockerArch = "linux-arm";
+IEnumerable<string> dockerArchs = new List<string>{
+    linuxX64DockerArch,
+    linuxArmDockerArch
+}.AsReadOnly();
+
 // This is the version of this software,
 // update before making a new release.
 const string version = "0.0.1";
+
+IEnumerable<string> dockerTags = new List<string>{ "latest", version }.AsReadOnly();
+const string dockerImageName = "xforever1313/cryptometheus";
 
 DotNetCoreMSBuildSettings msBuildSettings = new DotNetCoreMSBuildSettings();
 // Sets the assembly version.
@@ -88,11 +98,13 @@ void Publish( string runtime, DirectoryPath outputDir )
 
 // ---------------- Docker Targets ----------------
 
+// -------- Build --------
+
 Task( dockerLinuxX64Target )
 .Does(
     () =>
     {
-        BuildDocker( linuxX64DistDir );
+        BuildDocker( linuxX64DistDir, linuxX64DockerArch );
     }
 ).Description( "Builds the Linux x64 docker image" );
 
@@ -100,28 +112,95 @@ Task( dockerLinuxArmTarget )
 .Does(
     () =>
     {
-        BuildDocker( linuxArmDistDir );
+        BuildDocker( linuxArmDistDir, linuxArmDockerArch );
     }
 ).Description( "Builds the Linux arm docker image" );
 
-void BuildDocker( DirectoryPath distDir )
+void BuildDocker( DirectoryPath distDir, string arch )
 {
-    List<string> tags = new List<string> { "latest", version };
-    foreach( string tag in tags )
+    foreach( string tag in dockerTags )
     {
-        string arguments = $"build -t xforever1313/cryptometheus:{tag} -f docker/linux.dockerfile {distDir}";
+        string arguments = $"build -t {dockerImageName}:{tag}-{arch} -f docker/linux.dockerfile {distDir}";
         ProcessArgumentBuilder argumentsBuilder = ProcessArgumentBuilder.FromString( arguments );
-        ProcessSettings settings = new ProcessSettings
+        RunDocker( arguments );
+    }
+}
+
+Task( "docker_manifest" )
+.Does(
+    () =>
+    {
+        foreach( string tag in dockerTags )
         {
-            Arguments = argumentsBuilder
-        };
-        int exitCode = StartProcess( "docker", settings );
-        if( exitCode != 0 )
-        {
-            throw new ApplicationException(
-                "Error when running docker to build.  Got error: " + exitCode
-            );
+            StringBuilder arguments = new StringBuilder();
+            arguments.Append( $"manifest create {dockerImageName}:{tag}" );
+            foreach( string arch in dockerArchs )
+            {
+                arguments.Append( $" {dockerImageName}:{tag}-{arch}" );
+            }
+            ProcessArgumentBuilder argumentsBuilder = ProcessArgumentBuilder.FromString( arguments.ToString() );
+            RunDocker( argumentsBuilder );
         }
+    }
+).Description( "Creates the docker manifest.  Images must be built AND pushed first." );
+
+// -------- Push --------
+
+Task( "docker_push_linux" )
+.Does(
+    () =>
+    {
+        PushDocker( linuxX64DockerArch );
+    }
+).Description( "Pushes the linux x64 image.  Requires a user to be logged in first." );
+
+Task( "docker_push_linux_arm" )
+.Does(
+    () =>
+    {
+        PushDocker( linuxArmDockerArch );
+    }
+).Description( "Pushes the linux arm image.  Requires a user to be logged in first." );
+
+Task( "docker_push_manifest" )
+.Does(
+    () =>
+    {
+        PushDocker( null );
+    }
+).Description( "Pushes the manifest image.  Requires a user to be logged in first." );
+
+void PushDocker( string arch )
+{
+    if( string.IsNullOrWhiteSpace( arch ) == false )
+    {
+        arch = $"-{arch}";
+    }
+    else
+    {
+        arch = string.Empty;
+    }
+
+    foreach( string tag in dockerTags )
+    {
+        string arguments = $"push {dockerImageName}:{tag}{arch}";
+        ProcessArgumentBuilder argumentsBuilder = ProcessArgumentBuilder.FromString( arguments );
+        RunDocker( arguments );
+    }
+}
+
+void RunDocker( ProcessArgumentBuilder argumentsBuilder )
+{
+    ProcessSettings settings = new ProcessSettings
+    {
+        Arguments = argumentsBuilder
+    };
+    int exitCode = StartProcess( "docker", settings );
+    if( exitCode != 0 )
+    {
+        throw new ApplicationException(
+            "Error when running docker.  Got exit code: " + exitCode
+        );
     }
 }
 
--- jenkins/release.groovy
+++ jenkins/release.groovy
@@ -1,3 +1,6 @@
+// https://github.com/xforever1313/X13JenkinsLib
+@Library( "X13JenkinsLib" )_
+
 def CallCakeOnWindows( String cmd )
 {
     bat ".\\Cake\\dotnet-cake.exe .\\Cryptometheus\\build.cake ${cmd}"
@@ -11,80 +14,152 @@ def CallCakeOnUnix( String cmd )
 pipeline
 {
     agent none
+    parameters
+    {
+        booleanParam( name: "Deploy", defaultValue: true, description "Should we deploy as well?" );
+    }
     stages
     {
-        stage( 'build_linux_x64' )
+        stage( 'build' )
         {
-            agent
-            {
-                label "windows";
-            }
             stages
             {
-                stage( 'setup' )
+                stage( 'build_linux_x64' )
                 {
-                    steps
+                    agent
                     {
-                        bat "dotnet tool update Cake.Tool --tool-path .\\Cake"
-                        bat 'C:\\"Program Files"\\Docker\\Docker\\DockerCli.exe -Version';
-                        bat 'C:\\"Program Files"\\Docker\\Docker\\DockerCli.exe -SwitchLinuxEngine';
+                        label "windows";
+                    }
+                    stages
+                    {
+                        stage( 'setup' )
+                        {
+                            steps
+                            {
+                                bat "dotnet tool update Cake.Tool --tool-path .\\Cake"
+                                bat 'C:\\"Program Files"\\Docker\\Docker\\DockerCli.exe -Version';
+                                bat 'C:\\"Program Files"\\Docker\\Docker\\DockerCli.exe -SwitchLinuxEngine';
+                            }
+                        }
+                        stage( 'build_debug' )
+                        {
+                            steps
+                            {
+                                CallCakeOnWindows( "--target=build" );
+                            }
+                        }
+                        stage( 'publish_linux_x64' )
+                        {
+                            steps
+                            {
+                                CallCakeOnWindows( "--target=publish_linux" );
+                            }
+                        }
+                        stage( 'publish_linux_arm' )
+                        {
+                            steps
+                            {
+                                CallCakeOnWindows( "--target=publish_linux_arm" );
+                                stash includes: "Cryptometheus\\dist\\linux-arm\\**\\*", name: "linux_arm_build";
+                            }
+                        }
+                        stage( 'build_docker_x64' )
+                        {
+                            steps
+                            {
+                                CallCakeOnWindows( "--target=docker_linux" );
+                            }
+                        }
                     }
                 }
-                stage( 'build_debug' )
+                stage( 'build_linux_arm32' )
                 {
-                    steps
+                    agent
                     {
-                        CallCakeOnWindows( "--target=build" );
+                        label "linux && arm32";
                     }
-                }
-                stage( 'publish_linux_x64' )
-                {
-                    steps
+                    stages
                     {
-                        CallCakeOnWindows( "--target=publish_linux" );
-                    }
-                }
-                stage( 'publish_linux_arm' )
-                {
-                    steps
-                    {
-                        CallCakeOnWindows( "--target=publish_linux_arm" );
-                        stash includes: "Cryptometheus\\dist\\linux-arm\\**\\*", name: "linux_arm_build";
-                    }
-                }
-                stage( 'build_docker_x64' )
-                {
-                    steps
-                    {
-                        CallCakeOnWindows( "--target=docker_linux" );
+                        stage( 'setup' )
+                        {
+                            steps
+                            {
+                                sh "~/.dotnet/dotnet tool update Cake.Tool --tool-path ./Cake";
+                            }
+                        }
+                        stage( "build_docker_arm32" )
+                        {
+                            steps
+                            {
+                                unstash "linux_arm_build";
+                                CallCakeOnUnix( "--target=docker_linux_arm" );
+                            }
+                        }
                     }
                 }
             }
-        }
-        stage( 'build_linux_arm32' )
+        } // End Build
+        stage( 'deploy' )
         {
-            agent
-            {
-                label "linux && arm32";
-            }
             stages
             {
-                stage( 'setup' )
+                stage( 'deploy_linux_x64' )
                 {
-                    steps
+                    agent
                     {
-                        sh "~/.dotnet/dotnet tool update Cake.Tool --tool-path ./Cake";
+                        label "windows";
+                    }
+                    stages
+                    {
+                        stage( "Push" )
+                        {
+                            X13DockerLogin( "dockerhub" );
+                            CallCakeOnWindows( "--target=docker_push_linux" );
+                        }
                     }
                 }
-                stage( "build_docker_arm32" )
+                stage( 'deploy_linux_arm' )
                 {
-                    steps
+                    agent
                     {
-                        unstash "linux_arm_build";
-                        CallCakeOnUnix( "--target=docker_linux_arm" );
+                        label "linux && arm32";
+                    }
+                    stages
+                    {
+                        stage( "Push" )
+                        {
+                            X13DockerLogin( "dockerhub" );
+                            CallCakeOnUnix( "--target=docker_push_linux_arm" );
+                        }
+                    }
+                }
+                stage( "docker_manifest" )
+                {
+                    agent
+                    {
+                        label "linux && arm32";
+                    }
+                    stages
+                    {
+                        stage( "Build" )
+                        {
+                            CallCakeOnUnix( "--target=docker_manifest" );
+                        }
+                        stage( "deploy" )
+                        {
+                            X13DockerLogin( "dockerhub" );
+                            CallCakeOnUnix( "--target=docker_push_manifest" );
+                        }
                     }
                 }
             }
-        }
+            when
+            {
+                expression
+                {
+                    return params.Deploy;
+                }
+            }
+        } // End Deploy
     }
 }
